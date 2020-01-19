418f491900a94fea4499def75ffc31ca0d062127
JungEun <je991025@gmail.com>
2020-01-20 02:29:38 +0900

    정은 과제 2748

--- JungEun/2748.cpp
+++ JungEun/2748.cpp
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #pragma warning(disable:4996)
-
+/*6번만에 제출 성공함ㅋㅋㅋㅋㅋㅋㅋ
+ * long long 형식변환문자를 lld로 하지 않았다...
+ * 그것도 모르고 자꾸 애먼데를 수정하기를 다섯 번...
+ * 40분만에 깨.달.았.다.
+ * 기본에 충실하자!! 
+*/
 int main() {
 	int num;
 	long long *fib;
